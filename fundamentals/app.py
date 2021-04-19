5d57be68079f69b4d9160add802bc92aea521ed7
Alex Butakov <butakov.aleksey@gmail.com>
2021-04-18 17:34:26 -0700

    Initial commit today

--- fundamentals/app.py
+++ fundamentals/app.py
@@ -5,19 +5,36 @@
         Get familiar with tools - IN PROGRESS
         Get as far as a can in web-scraping course
 
+    Notes: 
+
 '''
 from lxml import etree
 
 tree = etree.parse("fundamentals/src/web_page.html")
-title_element = tree.find("body/ul/li")
+
 list_items = tree.findall("/body/ul/li")
 
-for li in list_items:
-    a = li.find("a")
-    if a is not None:
-        print(f"{li.text.strip()} {a.text}")
-    else:
-        print(li.text)
+# xpath testing
+list_elements = tree.xpath("//li")
+for li in list_elements:
+    text = li.xpath("//text()")
+    print(text)
+
+
+
+
+
+# print(etree.tostring(li))
+
+# Practice
+# print(title_element)
+
+# for li in list_items:
+#     a = li.find("a")
+#     if a is not None:
+#         print(f"{li.text.strip()} {a.text}")
+#     else:
+#         print(li.text)
 
 # print(list_items)
 
